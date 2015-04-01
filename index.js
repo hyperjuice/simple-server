6da1887e1d2b6edb7d14bdc5fc5f9c76ec20c659
Darrell Cohn <darrellcohn@gmail.com>
2015-03-31 18:52:00 -0700

    testing connection

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //for instructions on setting up an HTTP server: 
 //https://github.com/sf-wdi-17/notes/tree/master/lectures/week-03/_1_monday/dusk#url-parameters
 
+//
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+	res.send("<h1>This is a calculator</h1><p>to continue, type a URL in the address bar in this format: http://localhost:3000/math/multiply/40/5");
+});
 
 
 app.get('/math/add/:x/:y', function(req,res) { 
@@ -13,7 +17,7 @@ app.get('/math/add/:x/:y', function(req,res) {
 	var y = Number(req.params.y);
 	var result = x + y;
 	res.send("Result is " + result);
-
+//text
 
 });	
 
